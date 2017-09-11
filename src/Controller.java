836bf334e12b9de4871c6f7a6baf8a9eb9b7d0aa
pawe9N <zerratul@wp.pl>
2017-09-11 22:44:07 +0200

    I make almost everything
    I learn more about FXML, Controllers and added more css, more elements,
    some images and this project is about 500% better than yesterday!

--- src/Controller.java
+++ src/Controller.java
@@ -1,14 +1,46 @@
 
-
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+	
+	@FXML
+	AnchorPane anchor1, anchor2, anchor3, anchor4, anchor5, anchor6, anchor7, anchor8, anchor9;
+	
+	@FXML
+	Button button1, button2, button3, button4, button5, button6, button7, button8, button9;
 
-    @Override
+    @Override 
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("View is now loaded!");
+        
+        showMovieList();
+        
+    }
+    
+    public void anchorSetImage(AnchorPane anchor, String imgName){
+    	anchor.setStyle("-fx-background-image: url('images/" + imgName + ".jpg')");
+    }
+    
+    public void buttonSetText(Button button, String title){
+    	button.setText(title);;
+    }
+    
+    public void showMovieList(){
+        String [] imgNames = {"gladiator", "thegodfather", "americanbeauty", "goodfellas", "inception", "memento", "oneflewoverthecuckoosnest", "pulpfiction", "theprofesional"};
+        String [] titles = {"Gladiator", "Godfather", "American Beauty", "Goodfellas", "Iception", "Memento", "One flaw over the cuckoo's nest", "Pulp Fiction", "The Professional"};
+        AnchorPane [] imgVariables = {anchor1, anchor2, anchor3, anchor4, anchor5, anchor6, anchor7, anchor8, anchor9};
+        Button [] titleButtons = {button1, button2, button3, button4, button5, button6, button7, button8, button9};
+        
+        for(int i=0; i < imgVariables.length; i++){
+        	anchorSetImage(imgVariables[i], imgNames[i]);
+        	buttonSetText(titleButtons[i], titles[i]);
+        }
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	Scene scene;
 
     public static void main(String[] args) {
         launch(args);
@@ -12,13 +14,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+    	FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("Scene.fxml"));
         primaryStage.setTitle("Movie App");
-        
-        Scene scene = new Scene(root, 1080, 720);
+        Parent root = loader.load();
+        scene = new Scene(root, 1280, 720);
         scene.getStylesheets().add(getClass().getResource("Scene.css").toExternalForm());
+        scene.getStylesheets().add("https://fonts.googleapis.com/css?family=Bowlby+One+SC");
+        
         primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
