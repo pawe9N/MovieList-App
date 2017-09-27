d8e715567cdfc68213a0d3181c647ae673373786
pawe9N <zerratul@wp.pl>
2017-09-27 15:18:17 +0200

    Added sql file to make tables

---
+++ movieListApp.sql
@@ -0,0 +1,61 @@
+﻿-- MySQL dump 10.13  Distrib 8.0.1-dmr, for Win64 (x86_64)
+--
+-- Host: localhost    Database: movieApp
+-- ------------------------------------------------------
+-- Server version	8.0.1-dmr-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `movies`
+--
+
+DROP TABLE IF EXISTS `movies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `movies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL UNIQUE,
+  `imgName` varchar(255) DEFAULT NULL,
+  `description` text,
+  `premiere` int(11) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL,
+  `runtime` int(11) DEFAULT NULL,
+  `genre` varchar(50) DEFAULT NULL,
+  `watched` tinyint(1) DEFAULT NULL,
+  `toWatch` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+DROP TABLE IF EXISTS `series`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `series` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL UNIQUE,
+  `imgName` varchar(255) DEFAULT NULL,
+  `description` text,
+  `episodes` int(11) DEFAULT NULL,
+  `seasons` int(11) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL,
+  `genre` varchar(50) DEFAULT NULL,
+  `watched` tinyint(1) DEFAULT NULL,
+  `watching` tinyint(1) DEFAULT NULL,
+  `toWatch` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
